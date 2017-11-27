350d0a820557eeced4190c431abec23fdf6b4d80
JoffreyFerreira <jojo.ferreira@live.fr>
2017-11-27 15:56:01 +0100

    nettoyage du fichier et fonction de comparaison avec des commandes

--- serveur.c
+++ serveur.c
@@ -60,71 +60,21 @@ int main(int argc,char *argv[])
 
 /******************************************************************************/
 
-/* une couleur est represente par un char tel que :
-R = rouge
-Y = jaune
-G = vert
-B = bleu
-O = orange
-W = blanc
-V = violet
-*/
+/* peut etre utiliser pour parser les commandes */
+int compare_cmd(char *cmd, char *client, int n, int *bien_place, int *mal_place){
 
-char convert_int_coul(int num){ // on a besoin d'une fonction qui pour un entier entre 0 et 6 renvoie une couleur
-switch(num){
-	case 0 :
-	return('R');
-	case 1 :
-	return('Y');
-	case 2 :
-	return('G');
-	case 3 :
-	return('B');
-	case 4 :
-	return('O');
-	case 5 :
-	return('W');
-	default :
-	return('V');
-}
-}
-
-// permet de generer un entier aleatoire entre 0 et 6 compris
-int rand_a_b(int a, int b){
-	return rand()%(b-a) +a;
-}
-
-// on genere un entier aleatoire et on le convertit en une couleur grace aux fonctions du desssus
-void *cle_aleatoire(int n, char *cle){
-	for (int i = 0; i < n; ++i){
-		cle[i]= convert_int_coul(rand_a_b(0, 7));
-	}
-}
-
-/* permet de comparer les char des deux mots, on parcourt le mot envoyé par le client, si la couleur a l indice i est la meme pour le client
-et le serveur on incremente bien_place, sinon on regarde si la couleur de l indice i du client est presente ailleurs dans le mot du serveur
-avec une boucle for et on incremente mal_place si la couleur est presente ailleurs*/
-void compare(char *cle, char *client, int n, int *bien_place, int *mal_place){
-	/*printf("-----------------------\n");
-	printf("Je début la comparaison\n");
-	printf("-----------------------\n");*/
 	char lettre;
+	int compt = 1;
 	for (int i = 0; i < n; ++i){
-		lettre = client[i];;
-		if(client[i] == cle[i]){
-			//printf("J'ai trouvé une lettre bien placée\n");
-			(*bien_place)++;
+		lettre = client[i];
+		if(client[i] == cmd[i]){
+			compt++;
 		}
 		else{
-			for (int j = 0; j < n; ++j){
-				if(cle[j]==lettre){
-					//printf("J'ai trouvé une lettre mal placée\n");
-					(*mal_place)++;
-					break;
-				}
-			}
+			return 0;
 		}
 	}
+	return 1;
 }
 
 void serveur_appli(char *service)
@@ -149,16 +99,6 @@ void serveur_appli(char *service)
 	printf("ok\n");
 	n = 2*(atoi(taille_n))+1;
 
-	printf("Le client veut deviner %d couleurs\n", n);
-
-	char cle[n];
-	cle_aleatoire(n, cle);
-	printf("On a crée la chaine de couleur ci dessous\n");
-	for (int i = 0; i < n; ++i)
-	{
-		printf("%c", cle[i]);
-	}
-	printf("\n");
 	// on envoie les consignes au client
 	//printf("Message pret à envoyer\n");
 	h_writes(num_socket_client, "Rentrez une chaine de caractere :\nR = rouge\nY = jaune\nG = vert\nB = bleu\nO = orange\nW = blanc\nV = violet", 110);
@@ -182,7 +122,6 @@ void serveur_appli(char *service)
 		printf("\n");
 		bien_place = 0;
 		mal_place = 0;
-		compare(cle, client, n, &bien_place, &mal_place);
 		printf("J'ai comparé, bien place : %d et mal place : %d\n",bien_place,mal_place);
 		char tampon1[n];
 		int lg_tmp1;
@@ -207,8 +146,6 @@ void serveur_appli(char *service)
 		sprintf(tampon1, "%d", bien_place);
 		sprintf(tampon2, "%d", mal_place);
 		printf("\n");
-		/* on envoie le nombre de bien place et mal place, la gestion de la victoire est faite chez le client et le serveur
-		en comparant n et bien place */
 		h_writes(num_socket_client, lgtmp1, 1);
 		taille_effective = h_reads(num_socket_client, attente, 1);
 		h_writes (num_socket_client, tampon1, lg_tmp1) ; // on envoie
@@ -216,20 +153,11 @@ void serveur_appli(char *service)
 		h_writes(num_socket_client, lgtmp2, 1);
 		taille_effective = h_reads(num_socket_client, attente, 1);
 		h_writes (num_socket_client, tampon2, lg_tmp2) ;
-		if(bien_place==n){
-			printf("Le client a gagne au bout de %d coup !\n", nbr_coup);
-			gagne=1;
-		}
-		else{
-			printf("Le client a faux \n");
-			nbr_coup++;
-		}
 		char trans[1];
 		sprintf(trans, "%d", gagne);
 		h_writes(num_socket_client,trans,1);
 		h_close(num_socket_client);
 		h_close(num_socket);
-		//gagne = 1;
 	}
 
 }
